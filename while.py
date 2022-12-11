8564e30b33a2551e285106b2ccf0362a35fbf8c0
Artem <yarilin98@mail.ru>
2022-12-11 19:51:17 +0300

    11.12.22

---
+++ while.py
@@ -0,0 +1,27 @@
+x = int(input())
+kol = 0
+kol_ch = 0
+s = 0
+pr = 1
+maxim = 1
+minim = 9
+
+while x > 0:
+    last = x%10
+    kol = kol+1
+    if last%2 ==0:
+        kol_ch+=1
+    s=s+last
+    pr=pr*last
+    if last>maxim:
+        maxim = last
+    if last < minim:
+        minim = last
+    x = x//10
+
+    print('Всего цифр: ',kol)
+    print('Всего четных цифр: ', kol_ch)
+    print('Сумма всех цифр: ', s)
+    print('Произведение всех цифр: ', pr)
+    print('Maxim: ', maxim)
+    print('Minim: ', minim)
