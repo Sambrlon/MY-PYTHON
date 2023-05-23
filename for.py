f23c7a98f0609fc938c61477e9299b1e06cf694c
Sambrlon <yarilin98@mail.ru>
2023-05-23 18:39:50 +0300

    23.05

--- for.py
+++ for.py
@@ -20,8 +20,33 @@
 
 
 
-m, p, n = int(input()), int(input()), int(input())
+# m, p, n = int(input()), int(input()), int(input())
 
-for i in range(n):
-    print(i + 1, m)
-    m = m + p/100*m
+# for i in range(n):
+#     print(i + 1, m)
+#     m = m + p/100*m
+
+
+# expenses = [200, 300, 400, 500]
+# total = 0
+# for payment in expenses:
+#     total += payment
+# print(f'Всего потрачено: {total}')
+
+
+# medals = ['gold', 'silver', 'bronze', 'chocolate', 'chocolate', 'chocolate']
+# gold = 0
+# silver = 0
+# bronze = 0
+# chocolate = 0
+
+# for medal in medals:
+#     if medal == 'gold':
+#         gold += 1
+#     elif medal == 'silver':
+#         silver += 1
+#     elif medal == 'bronze':
+#         bronze += 1
+#     elif medal == 'chocolate':
+#         chocolate += 1
+# print(f'Всего медалей: {gold} золота, {silver} серебра, {bronze} бронзы, {chocolate} шоколадных')
