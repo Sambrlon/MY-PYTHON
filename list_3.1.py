66718fdafe5bbf16aaa5f5968225148491eec152
traviss_scottt <traviss_scottt@bwg-io.site>
2023-06-21 21:08:02 +0300

    :list_3.1

---
+++ list_3.1.py
@@ -0,0 +1,57 @@
+# animals = ['cat', 'dog', 'monkey']
+# print(animals)
+
+
+# print(type(['cat', 'dog', 'monkey']))
+
+
+# hp_books = ['The Hobbit', 'The Lord of the Rings', 'The Silmarillion']
+# print(hp_books)
+# print(hp_books[2])
+
+
+# weather = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+# date = int(input('Введите день: '))
+
+# print(weather[date - 1])
+
+
+
+# print(len(['cat', 'dog', 'monkey']))
+
+
+
+# brhd_ring = ['The Hobbit', 'The Lord of the Rings', 'The Silmarillion']
+# brhd_ring.append('The Shire')
+
+# print(brhd_ring)
+
+
+
+# shopping_list = ['яблоки', 'молоко', 'помидоры']
+
+# destination = input('Куда пойдем?: ')
+
+# if destination == 'пикник':
+#     # shopping_list.append('пиво')
+#     # shopping_list.append('мороженое')
+#     shopping_list.extend(['пиво', 'мороженое'])
+# elif destination == 'в гости':
+#     shopping_list.append('вино')
+#     shopping_list.append('торт')
+# elif destination == 'домой':
+#     shopping_list.append('мармелад')
+#     shopping_list.append('лимонад')
+# else:
+#     print('Такого места нет')
+
+# print(f'Ваша корзина: {len(shopping_list)} товара, {shopping_list}')
+
+
+
+
+
+
+
+
