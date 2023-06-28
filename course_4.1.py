1c1047e731c4a87926d11aef78e36eb73e6b4f9e
traviss_scottt <traviss_scottt@bwg-io.site>
2023-06-28 20:22:58 +0300

    4.1

--- course_4.1.py
+++ course_4.1.py
@@ -18,7 +18,7 @@
 # print(s[0:5])
 # ---------------------------------
 
-text = 'Алиса привет, как твои делишки?'
+# text = 'Алиса привет, как твои делишки?'
 
 # if len(text) > 10:
 #     print('Длина текста больше 10')
@@ -32,22 +32,43 @@ text = 'Алиса привет, как твои делишки?'
 #     print(i)
 # ---------------------------------
 
-string = 'Алиса привет, как твои делишки?'
+# string = 'Алиса привет, как твои делишки?'
 
-letters_count = 0
-words_count = 0
-spaces_count = 0
+# letters_count = 0
+# words_count = 0
+# spaces_count = 0
 
-for letter in string:
-    if letter not in [',', '.', '?', '!']:
-        letters_count += 1
-    elif letter == ' ':
-        spaces_count += 1
+# for letter in string:
+#     if letter not in [',', '?', '!']:
+#         letters_count += 1
+#     elif letter == ' ':
+#         spaces_count += 1
 
-if spaces_count > 0:
-    words_count = spaces_count + 1
-elif letters_count > 0:
-    words_count += 1
+# if spaces_count > 0:
+#     words_count = spaces_count + 1
+# elif letters_count > 0:
+#     words_count = 1
 
-print(f'Колличество букв - {letters_count}')
-print(f'Колличество слов - {words_count}') 
+# print(f'Колличество букв - {letters_count}')
+# print(f'Колличество слов - {words_count}') 
+# --------------------------------
+
+# email = 'lobinova@bk.ru'
+# print(email.count('@')) 
+
+# count - кол-во входжений
+# ------------------------------------
+
+# print('У меня в тексте есть Бльшие Буквы, А В ДРУГИХ НЕТ'.lower())
+
+## .lower - метод для отображения слов в нижнем регистре
+# ----------------------------------
+
+# email = 'my new email lobinova@bk.ru'
+# new_email = email.replace(' ', '_')
+# print(new_email)
+# print(email)
+
+## .replace - заменяет все вхождения в строке на другую
+# -----------------------------------
+
